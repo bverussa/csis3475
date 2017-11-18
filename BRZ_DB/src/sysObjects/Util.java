557db63d67750f302e024537e2623af40101a2e5
crist <crist@10.1.0.112>
2017-11-18 14:41:07 -0800

    Static fields for database master, user admin and table users.

--- BRZ_DB/src/sysObjects/Util.java
+++ BRZ_DB/src/sysObjects/Util.java
@@ -9,7 +9,7 @@ public class Util
 {
 	public static String DB_MASTER = "master";
 	public static String TBL_USER = "tblUser";
-	public static String USER_ADMIN = "admin";
+	public static String USER_ADMIN = "admin"; 
 	
 	public static void addComponent(JFrame frame, Component c, int x, int y, int w, int h) 
 	{
