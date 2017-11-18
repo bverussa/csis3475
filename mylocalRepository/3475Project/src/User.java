42268254f9d2c48f23be0b917bda45fd9e736964
VictorLuis <VictorLuis@10.1.34.125>
2017-11-18 13:21:46 -0800

    Add user class

---
+++ mylocalRepository/3475Project/src/User.java
@@ -0,0 +1,107 @@
+import java.awt.Component;
+import java.net.PasswordAuthentication;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.plaf.basic.BasicBorders.RadioButtonBorder;
+
+public class User extends JFrame {
+	
+	private JLabel jlName,jlPassword,jlConfirm;
+	private static JTextField tfName;
+	private static JTextField tfPassword;
+	private static JTextField tfConfirm;
+	private static JRadioButton rbAdmin, rbUser;
+	private static ButtonGroup bgGroup;
+	private static JButton btnCreate;
+	private static String name;
+	private static String password;
+	private static String user;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+		this.setVisible(true);
+		this.setBounds(30,40,400,300);
+		this.setLayout(null);
+		
+		jlName = new JLabel("Name: ");
+		addMe(jlName, 10, 10, 70, 20);
+		
+		jlPassword = new JLabel("Password: ");
+		addMe(jlPassword, 9, 35, 70, 20);
+		
+		jlConfirm = new JLabel("Confirm: ");
+		addMe(jlConfirm, 10, 60, 70, 20);
+		
+		tfName = new JTextField();
+		addMe(tfName, 75, 10, 150, 20);
+		
+		tfConfirm = new JTextField();
+		addMe(tfConfirm, 75, 60, 150, 20);
+		
+		tfPassword = new JTextField();
+		addMe(tfPassword, 75, 35, 150, 20);
+		
+		rbAdmin = new JRadioButton("Administrator");
+		addMe(rbAdmin,230, 10, 120, 20);
+		
+		rbUser = new JRadioButton("User");
+		addMe(rbUser,230, 30, 120, 20);
+		
+		btnCreate = new JButton("Create");
+		addMe(btnCreate, 30, 80, 90, 20);
+		
+		bgGroup = new ButtonGroup();
+		bgGroup.add(rbAdmin);
+		bgGroup.add(rbUser);
+	}
+	
+	
+	private void addMe(Component c, int x, int y, int w, int h){
+		add(c);
+		c.setBounds(x,y,w,h);
+	}
+	
+	public static void main(String args[]){
+		new User();
+		
+	}
+	
+	//|| !tfPassword.getText().trim().equals("")
+	public static void validateUser(){
+		
+		ReturnValue r = new ReturnValue();
+		
+		if(!(tfName.getText()==null) ){
+			name = tfName.getText();
+		}else{
+			r.msg = "Invalid User Name. Please check if you entered correct name .";
+		}if(!(tfPassword.getText()==null) ){
+			if(!(tfConfirm.getText()==null)){
+				if(tfPassword.getText().equals(tfConfirm.getText())){
+					password = tfPassword.getText();
+				}else{
+					r.msg = "Password does not match . Please check if you entered correct password  .";
+				}
+			}else{
+				r.msg = "Invalid confirmation . Please check if you entered correct password  .";
+			}
+		}
+			else{
+			r.msg = "Invalid Password . Please check if you entered correct password  .";
+		}if(!(bgGroup.getSelection()==null)){
+			user = bgGroup.getSelection().toString();
+		}
+		r.msg = "Invalid User Type . Please check if you have selected the user type  .";
+	}
+	
+	public static void InsertUser(String name,String password, String userType){
+		Insert.run("INSERT INTO tblUser values("+name+","+password+","+userType+")", "master");
+		
+	}
+
+}
