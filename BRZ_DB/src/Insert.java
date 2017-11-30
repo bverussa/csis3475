8591e9ecb5432fd4dfc710b3c3a83357e34384f8
Bruno Verussa <b.verussa@gmail.com>
2017-11-29 18:05:00 -0800

    Add control to avoid usertype 2 to insert to tblUser

--- BRZ_DB/src/Insert.java
+++ BRZ_DB/src/Insert.java
@@ -64,64 +64,72 @@ public class Insert
         		r.msg = "Error to parse the query.";
         	else
         	{
-	        	// match the columns x values
-	        	if(columns.size() != values.size())
-	        		r.msg = "The number of columns does not correspond to the number of values. Please check your query.";
-	        	else 
+	        	if ((tableName.equals("tblUser") && ClientApplication.userType == 1) || (!tableName.equals("tblUser") && ClientApplication.userType > 0))
 	        	{
-	        		Table tbl = TableFile.readTable(databaseName, tableName);
-	        		
-	        		if (tbl == null)
-	        			r.msg = "Error on table configuration. Please check the table file.";
-	        		else 
-	        		{
-                    	// match the columns in the query with the file 
-        				if (!TableFile.columnsValid(tbl, columns))
-        					r.msg = "Invalid column. Please check your command.";
-        				else 
-        				{
-	                    	ArrayList<String> finalValues = new ArrayList<String>();
-	                    	
-	                		// fill the values of the columns not included in the query (null)
-	                    	// and put the values in the same order as the columns in the file
-	                    	int index;
-	                    	
-	                    	for (int i = 0; i < tbl.columns.size(); i++)
-	                    	{
-	                    		index = columns.indexOf(tbl.columns.get(i).toUpperCase());
-	                    		if (index == -1) // if the column was not included in the query
-	                    			finalValues.add("NULL");
-	                    		else
-	                    			finalValues.add(values.get(index));
-	                    	}
-	                    	
-	                    	// match the data type in the query with the file
-	                    	// create the string line with the values of the query
-	                    	boolean typeValid = true;
-	                    	
-	                    	String writeValues = "";
-	                    	for (int j = 0; j < finalValues.size(); j++) 
-	                    	{
-	                    		typeValid = DataType.isValid(tbl.types.get(j), finalValues.get(j));
-	                    		if(!typeValid) break;
-	                    		else writeValues += finalValues.get(j) + "|";
-	                    	}
-	                    	
-	                    	if(!typeValid) 
-	                    		r.msg = "Invalid data type.";
-	                    	else 
-	                    	{
-		                		// write the line in the file
-	                    		if(TableFile.writeLine(databaseName, tableName, writeValues))
-	                    		{
-	                    			r.msg = "Query executed successfully!";
-	                    			r.success = true;
-	                    		}
-	                    		else
-	                    			r.msg = "Error to execute the query.";
-	                    	}
-        				}
-	        		}
+	        		// match the columns x values
+		        	if(columns.size() != values.size())
+		        		r.msg = "The number of columns does not correspond to the number of values. Please check your query.";
+		        	else 
+		        	{
+		        		Table tbl = TableFile.readTable(databaseName, tableName);
+		        		
+		        		if (tbl == null)
+		        			r.msg = "Error on table configuration. Please check the table file.";
+		        		else 
+		        		{
+	                    	// match the columns in the query with the file 
+	        				if (!TableFile.columnsValid(tbl, columns))
+	        					r.msg = "Invalid column. Please check your command.";
+	        				else 
+	        				{
+		                    	ArrayList<String> finalValues = new ArrayList<String>();
+		                    	
+		                		// fill the values of the columns not included in the query (null)
+		                    	// and put the values in the same order as the columns in the file
+		                    	int index;
+		                    	
+		                    	for (int i = 0; i < tbl.columns.size(); i++)
+		                    	{
+		                    		index = columns.indexOf(tbl.columns.get(i).toUpperCase());
+		                    		if (index == -1) // if the column was not included in the query
+		                    			finalValues.add("NULL");
+		                    		else
+		                    			finalValues.add(values.get(index));
+		                    	}
+		                    	
+		                    	// match the data type in the query with the file
+		                    	// create the string line with the values of the query
+		                    	boolean typeValid = true;
+		                    	
+		                    	String writeValues = "";
+		                    	for (int j = 0; j < finalValues.size(); j++) 
+		                    	{
+		                    		typeValid = DataType.isValid(tbl.types.get(j), finalValues.get(j));
+		                    		if(!typeValid) break;
+		                    		else writeValues += finalValues.get(j) + "|";
+		                    	}
+		                    	
+		                    	if(!typeValid) 
+		                    		r.msg = "Invalid data type.";
+		                    	else 
+		                    	{
+			                		// write the line in the file
+		                    		if(TableFile.writeLine(databaseName, tableName, writeValues))
+		                    		{
+		                    			r.msg = "Query executed successfully!";
+		                    			r.success = true;
+		                    		}
+		                    		else
+		                    			r.msg = "Error to execute the query.";
+		                    	}
+	        				}
+		        		}
+		        	}
+	        	}
+	        	else
+	        	{
+	        		r.success = false;
+	        		r.msg = "Only adminstrators are allowed to create new users. \nPlease, contact the administrator";
 	        	}
 	        }
     	}
